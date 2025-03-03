23a55fcf459ffdac713b271acc06d3f7a199da61
marketlube <marketlubes@gmail.com>
2025-03-03 11:41:46 +0530

    .env updated

--- src/axios/axiosInstance.js
+++ src/axios/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: `${process.env.BACKEND_URL}/api/v1`,
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api/v1`,
   withCredentials: true,
 });
 
